ccdca5a82554229c849e83ef8cf1400b6223f29a
pavel <pvlapin@mail.ru>
2023-11-22 16:55:43 +0700

    задание 1

---
+++ inh_ex1.py
@@ -0,0 +1,44 @@
+import math
+
+
+class Dot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.color = ''
+
+    def __str__(self):
+        return f"x = {self.x}, y = {self.y}, color = {self.color}"
+
+    def set_color(self, color = 'white'):
+        self.color = color
+
+class Circle(Dot):
+    def __init__(self, x=0, y=0, r=1):
+        super().__init__(x, y)
+        self.r = r
+
+    def __str__(self):
+        return f"x = {self.x}, y = {self.y}, r = {self.r}, color = {self.color}"
+
+    def area(self):
+        return math.pi * self.r ** 2
+
+    def set_radius(self, r):
+        self.r = r
+
+class Sphere(Circle):
+    def __init__(self, x, y, r):
+        super().__init__(x, y, r)
+
+    def volume(self):
+        return super().area()
+
+    def area(self):
+        pass
+    
+c = Circle(1, 2, 3)
+print(c.area())
+s = Sphere(1, 1, 3)
+print(s.area())
+print(s.volume())
